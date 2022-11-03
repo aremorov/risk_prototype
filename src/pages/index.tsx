a0e2b0b0c8d0a4c5b38a63f1217c663f405c47de
Andrey <remorov.andrey@gmail.com>
2022-11-03 15:12:44 -0400

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <div>Hello React</div>
+      <div>Hello React</div>
+      <svg height="100" width="500">
+        <ellipse cx="240" cy="50" rx="220" ry="30" />
+        <ellipse cx="220" cy="50" rx="190" ry="20" />
+      </svg>
+    </div>
+  );
+};
+
+export default HomePage;
